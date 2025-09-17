db11a0eabdb8bbd0f626e85d202ed8c332334b5b
Vinay730 <vinay.juris@gmail.com>
2025-09-17 16:12:41 +0100

    add color

--- src/common/Footer.jsx
+++ src/common/Footer.jsx
@@ -74,10 +74,10 @@ export default function Footer() {
         <div>
           <h3 className="text-white font-semibold mb-4">Follow Us</h3>
           <div className="flex gap-4">
-            <a href="#" className="hover:text-blue-400">
+            <a href="#" className="hover:text-blue-400 bg-blue-700 rounded">
               <Facebook />
             </a>
-            <a href="#" className="hover:text-blue-400">
+            <a href="#" className="hover:text-red-700 bg-red-500 rounded">
               <Instagram />
             </a>
           </div>
